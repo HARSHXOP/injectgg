bfcd4bb9735723c2043f619fa39888ac3f3d1290
HARSHXOP <78731731+HARSHXOP@users.noreply.github.com>
2021-07-30 17:13:53 +0530

    Update gg_base_values.lua

--- gg_base_values.lua
+++ gg_base_values.lua
@@ -1,1 +1,36 @@
-gg.alert("000")
+--____________________________________________________--
+
+--  BYPASS EDIT VALUES --
+
+__ ANDEQ SLS # __
+144,387
+131,330
+
+
+
+
+
+__ STREQ __
+
+67109633
+67109377
+769
+83886849
+
+
+
+
+
+
+
+
+
+
+
+
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("ooo",gg.TYPE_DWORD)
+gg.refineNumber("iii",gg.TYPE_DWORD)
+gg.getResults("100")
+gg.editAll("ooo",gg.TYPE_DWORD)
+
